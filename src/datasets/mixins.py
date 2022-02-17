cb54638996ab40cfb73cef23acbee63210a10a4f
Krzysztof Zajac <kzajac1997@gmail.com>
2022-02-17 16:54:36 +0100

    Add ChunkMixin

--- src/datasets/mixins.py
+++ src/datasets/mixins.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from functools import cached_property
+from typing import List, Sequence, Tuple
 
 import numpy as np
 import torch
@@ -22,13 +23,13 @@ class TorchDataLoaderMixin(ABC):
 
     @cached_property
     @abstractmethod
-    def train(self):
+    def train(self) -> Sequence:
         """Must be inherited from SequenceGenerator"""
         ...
 
     @cached_property
     @abstractmethod
-    def test(self):
+    def test(self) -> Sequence:
         """Must be inherited from SequenceGenerator"""
         ...
 
@@ -54,3 +55,47 @@ class TorchDataLoaderMixin(ABC):
         dataset = self._prepare_dataset(features, targets)
 
         return DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+
+
+class ChunkMixin(ABC):
+    """Mixing adding functionality to split time series dataset using any ratio"""
+    def __init__(self, use_test_split: bool = True):
+        """
+        :param use_test_split: if True use testing setting in split
+        """
+        self.use_test_split = use_test_split
+
+    @property
+    @abstractmethod
+    def data(self) -> Sequence:
+        """Must be inherited from SequenceGenerator"""
+        ...
+
+    def split(self, values: np.array, test: bool = False) -> Tuple[np.array, ...]:
+        """Must be inherited from SequenceGenerator"""
+        ...
+
+    def _chunk(self, indices: np.array) -> List:
+        """Chunks data using given splitting indices"""
+        start = 0
+        slices = []
+
+        for end in indices:
+            slices.append(self.split(self.data[start:end], test=self.use_test_split))
+            start = end
+
+        return slices
+
+    def chunk_by_ratios(self, ratios: Tuple[float, ...]) -> List:
+        """Chunk data by ratios relative to its length"""
+        if sum(ratios) != 1:
+            raise ValueError(f"Ratios must add up to 1! {sum(ratios)} != 1")
+
+        return self._chunk((np.asarray(ratios) * len(self.data)).astype(int))
+
+    def chunk_by_samples(self, samples: Tuple[int, ...]) -> List:
+        """Chunk data by sample indices to its length"""
+        if sum(samples) != len(self.data):
+            raise ValueError(f"Samples must divide Sequence! {sum(samples)} != {len(self.data)}")
+
+        return self._chunk(np.asarray(samples).astype(int))
